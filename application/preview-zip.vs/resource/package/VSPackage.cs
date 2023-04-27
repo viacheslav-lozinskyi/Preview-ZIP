a0446805a5661775ec14115a757b57bc9a3b50da
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-04-27 17:56:34 +0300

    - Minor changes

--- application/preview-zip.vs/resource/package/VSPackage.cs
+++ application/preview-zip.vs/resource/package/VSPackage.cs
@@ -20,6 +20,7 @@ namespace resource.package
         internal static class CONSTANT
         {
             public const string APPLICATION = "Visual Studio";
+            public const string COMPANY = "Viacheslav Lozinskyi";
             public const string COPYRIGHT = "Copyright (c) 2020-2023 by Viacheslav Lozinskyi. All rights reserved.";
             public const string DESCRIPTION = "Quick preview of ZIP files";
             public const string GUID = "3D481846-362D-429E-AFF7-0BF74A594CF9";
