f8d69bb4a1b77354bcc0687ca85d9e5fc6121910
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-05-04 19:39:47 +0300

    - Minor changes

--- application/preview-zip.vs/resource/package/VSPackage.cs
+++ application/preview-zip.vs/resource/package/VSPackage.cs
@@ -26,7 +26,7 @@ namespace resource.package
             public const string GUID = "3D481846-362D-429E-AFF7-0BF74A594CF9";
             public const string HOST = "MetaOutput";
             public const string NAME = "Preview-ZIP";
-            public const string VERSION = "1.0.13";
+            public const string VERSION = "1.1.0";
         }
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
